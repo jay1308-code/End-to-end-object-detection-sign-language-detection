fcf0154f776515de252150f096757ece7b3c1f39
Jay Pandya <jpandya065@gmail.com>
2024-06-23 11:15:00 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'Jay Pandya',
+    author_email= 'jpandya065@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
